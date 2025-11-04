3891fcc74a3f0ab0ffa7cb1565f504267e1872bb
TimothyWaniayeChristian <tcwaniaye@gmail.com>
2025-11-04 14:54:40 +0300

    Portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
 
-import About from "./pages/About";
-import Services from "./pages/Services";
-import Resume from "./pages/Resume";
-import Skills from "./pages/Skills";
-import Projects from "./pages/Projects";
-import Contact from "./pages/Contact";
+// import About from "./pages/About";
+// import Services from "./pages/Services";
+// import Resume from "./pages/Resume";
+// import Skills from "./pages/Skills";
+// import Projects from "./pages/Projects";
+// import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -18,12 +18,12 @@ function App() {
 
         <Routes>
           <Route path="/" element={<Hero />} />
-          <Route path="/about" element={<About />} />
+          {/* <Route path="/about" element={<About />} />
           <Route path="/services" element={<Services />} />
           <Route path="/resume" element={<Resume />} />
           <Route path="/skills" element={<Skills />} />
           <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<Contact />} /> */}
         </Routes>
 
         <Footer />
